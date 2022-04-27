357acb64094a34fa7d11ee361cbec0f9e97a22cc
LucasMaciel404 <maciellucas487@gmail.com>
2022-04-26 22:30:11 -0300

    26/4

---
+++ app/action/resources.php
@@ -0,0 +1,40 @@
+<?php
+require_once __DIR__ . "/../db/database.php";
+function tabela()//funçao tabela(); imprime a tabela na minha tela com os valores do banco de dados;
+{
+    $a = new Database; //colocando a classe Database em uma variavel;
+    $resultado = $a->select();// a funçao select me retorna um array associativo com os dados do banco de dados (tendo a chave primaria como chave do array); 
+    $cont = 0;//variavel criada para listar os numeros em sequencia;
+    foreach($resultado as $key)//ira percorrer meu banco de dados;
+    {
+        $cont += 1;//contador ira receber sempre +1;
+        $id = $key['id'];//recebera o id para mandar por via $_GET;
+        $nome = $key['nome'];//pega a variavel nome da minha tabela;
+        $gmail = $key['gmail'];//pega a variavel gmail do meu banco de dados;
+        echo"   <tr><th scope='row'>$cont</th>
+                    <td>$nome</td>
+                    <td>$gmail</td>
+                    <td class='bla'><a class='btn2' href='/view/php/editar.php?nid=$id'>Editar</a></td>
+                    <td class='bla'><a class='btn1' href='/view/php/excluir.php?nid=$id'>Excluir</a></td>
+                </tr>"; //vai exibir conteudo em uma tabela stilizada;
+    }
+
+}
+function excluir($id)//exclue uma informaçao da tabela;
+{
+    $a = new Database;
+    $a->delet($id);//usa a funçao da minha classe database;
+}
+function editar()//edita uma variavel especifica;
+{
+    require_once __DIR__ . "/../../app/db/database.php";
+    $con = new database;
+    $id = $_GET['nid'];//recebe uma variavel nid repassada pelo metodo get;
+    $nome = $_POST['nome']?? null;//recebe a variavel nome pelo metodo post;
+    $curso = $_POST['curso']?? null;//recebe a variavel curso pelo metodo post;
+    if (isset($_POST['editar']) and isset($_POST['nome']) and isset($_POST['gmail']))//verifica se há conteudo nas variaveis;
+    {
+        $con->update($id, $nome,$curso);//atualiza os valores em suas respectivas variaveis;
+        header('location: /index.php');//retorna o usuario para a tela inicial;
+    }
+}
---
+++ view/php/editar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="/view/style/editar.css">
+    <title>Login</title>
+</head>
+<body>
+    <!-- FORMULARIO DE METODO POST -->
+    <form class='box' method='POST' action='#'>
+        <h1>Editar</h1>
+        <input type='text' placeholder='nome' value = "<?php  ?>" name="nome">
+        <input type="email" placeholder="gmail" name="gmail"> <br>
+        <input type="submit" value = "editar" name="editar">
+        <a class="blaa"href="/index.php">voltar</a>
+    </form>
+</body>
+</html>
+<?php
+require_once __DIR__ . "/../../app/action/resources.php";
+editar();
